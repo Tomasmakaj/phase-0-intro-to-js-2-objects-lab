0d7f32620ef77c29db29e61d0f2c1f56f1b49b8a
Tomas Makaj <Tmakajcoding@gmail.com>
2022-07-04 13:26:31 -0400

    commit tree 09b33524e862bbc82aa0339695916078aae07afb

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+
+
+
+
+
+// const name = {};
+// const address = {
+//     street: {
+//         line1: ["41 Tyndale Pl"],
+//         line2: ["2nd Floor"],
+//     },
+//     city: ["Yonkers"],
+//     state: ["NY"],
+//     zipCode: ["10701"],
+// }
